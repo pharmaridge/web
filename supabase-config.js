0765c60c9363ea6f646aa49bc7dde72eaab2e32b
PharmaRidge <hello@pharmaridge.com>
2026-09-08 11:37:08 +0000

    Connect production admin portal to Supabase project

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.PHARMA_SUPABASE = {
+  url: 'https://oenowtbwinhrqkbjdjjo.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lbm93dGJ3aW5ocnFrYmpkampvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4NjE5MjIsImV4cCI6MjEwNDQzNzkyMn0.wzRAnbyWDCd1CNDALhnKscBTmxNK9su_CugPUBVk9WA'
+};
